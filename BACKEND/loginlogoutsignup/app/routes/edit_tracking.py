21f56bac2044a6a0c931d4fe713e9d30f43fc726
anushkahmed <anushka.ahmed@g.bracu.ac.bd>
2026-04-20 07:53:36 +0600

    Final update

--- BACKEND/loginlogoutsignup/app/routes/edit_tracking.py
+++ BACKEND/loginlogoutsignup/app/routes/edit_tracking.py
@@ -10,7 +10,7 @@ from app.utils.helpers import success, error, get_current_user
 
 edit_tracking_bp = Blueprint("edit_tracking", __name__)
 
-HF_API_KEY = os.getenv("HF_API_KEY") or "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"
+HF_API_KEY = os.getenv("HF_API_KEY", "")
 
 
 class Submission(db.Model):
